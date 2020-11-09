12ab591f093ce21c6490ea7d15fa71c15f310dbf
Thomas <trich1230@gmail.com>
2020-11-09 13:12:46 -0800

    updating site

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./Home.svelte";
+  import Nav from "./Nav.svelte";
+
+  // import About from "./About.svelte";
+  // import Blog from "./Blog.svelte";
+
+  export let url = "";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  
 </style>
+
+<Router {url}>
+  <Nav />
+
+  <div>
+    <!-- <Route path="blog/:id" component="{BlogPost}" />
+    <Route path="blog" component="{Blog}" />
+    <Route path="about" component="{About}" /> -->
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
