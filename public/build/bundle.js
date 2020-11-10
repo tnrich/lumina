75506e9b77fcad3956c56bcbf59b155a484cde4d
Thomas <trich1230@gmail.com>
2020-11-09 16:50:10 -0800

    adding email mailing list

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -293,6 +293,12 @@ var app = (function () {
         }
     }
 
+    const globals = (typeof window !== 'undefined'
+        ? window
+        : typeof globalThis !== 'undefined'
+            ? globalThis
+            : global);
+
     function get_spread_update(levels, updates) {
         const update = {};
         const to_null_out = {};
@@ -485,6 +491,10 @@ var app = (function () {
             dispatch_dev('SvelteDOMRemoveAttribute', { node, attribute });
         else
             dispatch_dev('SvelteDOMSetAttribute', { node, attribute, value });
+    }
+    function prop_dev(node, property, value) {
+        node[property] = value;
+        dispatch_dev('SvelteDOMSetProperty', { node, property, value });
     }
     function validate_slots(name, slot, keys) {
         for (const slot_key of Object.keys(slot)) {
@@ -2043,10 +2053,184 @@ var app = (function () {
     	}
     }
 
+    function createCommonjsModule(fn, basedir, module) {
+    	return module = {
+    	  path: basedir,
+    	  exports: {},
+    	  require: function (path, base) {
+          return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+        }
+    	}, fn(module, module.exports), module.exports;
+    }
+
+    function commonjsRequire () {
+    	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
+    }
+
+    var EmailJSResponseStatus_1 = createCommonjsModule(function (module, exports) {
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.EmailJSResponseStatus = void 0;
+    var EmailJSResponseStatus = /** @class */ (function () {
+        function EmailJSResponseStatus(httpResponse) {
+            this.status = httpResponse.status;
+            this.text = httpResponse.responseText;
+        }
+        return EmailJSResponseStatus;
+    }());
+    exports.EmailJSResponseStatus = EmailJSResponseStatus;
+    });
+
+    var UI_1 = createCommonjsModule(function (module, exports) {
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.UI = void 0;
+    var UI = /** @class */ (function () {
+        function UI() {
+        }
+        UI.clearAll = function (form) {
+            form.classList.remove(this.PROGRESS);
+            form.classList.remove(this.DONE);
+            form.classList.remove(this.ERROR);
+        };
+        UI.progressState = function (form) {
+            this.clearAll(form);
+            form.classList.add(this.PROGRESS);
+        };
+        UI.successState = function (form) {
+            form.classList.remove(this.PROGRESS);
+            form.classList.add(this.DONE);
+        };
+        UI.errorState = function (form) {
+            form.classList.remove(this.PROGRESS);
+            form.classList.add(this.ERROR);
+        };
+        UI.PROGRESS = 'emailjs-sending';
+        UI.DONE = 'emailjs-success';
+        UI.ERROR = 'emailjs-error';
+        return UI;
+    }());
+    exports.UI = UI;
+    });
+
+    var source = createCommonjsModule(function (module, exports) {
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.EmailJSResponseStatus = exports.sendForm = exports.send = exports.init = void 0;
+
+    Object.defineProperty(exports, "EmailJSResponseStatus", { enumerable: true, get: function () { return EmailJSResponseStatus_1.EmailJSResponseStatus; } });
+
+    var _userID = null;
+    var _origin = 'https://api.emailjs.com';
+    function sendPost(url, data, headers) {
+        if (headers === void 0) { headers = {}; }
+        return new Promise(function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.addEventListener('load', function (event) {
+                var responseStatus = new EmailJSResponseStatus_1.EmailJSResponseStatus(event.target);
+                if (responseStatus.status === 200 || responseStatus.text === 'OK') {
+                    resolve(responseStatus);
+                }
+                else {
+                    reject(responseStatus);
+                }
+            });
+            xhr.addEventListener('error', function (event) {
+                reject(new EmailJSResponseStatus_1.EmailJSResponseStatus(event.target));
+            });
+            xhr.open('POST', url, true);
+            for (var key in headers) {
+                xhr.setRequestHeader(key, headers[key]);
+            }
+            xhr.send(data);
+        });
+    }
+    function appendGoogleCaptcha(templatePrams) {
+        var element = document && document.getElementById('g-recaptcha-response');
+        if (element && element.value) {
+            templatePrams['g-recaptcha-response'] = element.value;
+        }
+        element = null;
+        return templatePrams;
+    }
+    function fixIdSelector(selector) {
+        if (selector[0] !== '#') {
+            return '#' + selector;
+        }
+        return selector;
+    }
+    /**
+     * Initiation
+     * @param {string} userID - set the EmailJS user ID
+     * @param {string} origin - set the EmailJS origin
+     */
+    function init(userID, origin) {
+        _userID = userID;
+        _origin = origin || 'https://api.emailjs.com';
+    }
+    exports.init = init;
+    /**
+     * Send a template to the specific EmailJS service
+     * @param {string} serviceID - the EmailJS service ID
+     * @param {string} templateID - the EmailJS template ID
+     * @param {Object} templatePrams - the template params, what will be set to the EmailJS template
+     * @param {string} userID - the EmailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
+     */
+    function send(serviceID, templateID, templatePrams, userID) {
+        var params = {
+            lib_version: '2.6.3',
+            user_id: userID || _userID,
+            service_id: serviceID,
+            template_id: templateID,
+            template_params: appendGoogleCaptcha(templatePrams)
+        };
+        return sendPost(_origin + '/api/v1.0/email/send', JSON.stringify(params), {
+            'Content-type': 'application/json'
+        });
+    }
+    exports.send = send;
+    /**
+     * Send a form the specific EmailJS service
+     * @param {string} serviceID - the EmailJS service ID
+     * @param {string} templateID - the EmailJS template ID
+     * @param {string | HTMLFormElement} form - the form element or selector
+     * @param {string} userID - the EmailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
+     */
+    function sendForm(serviceID, templateID, form, userID) {
+        if (typeof form === 'string') {
+            form = document.querySelector(fixIdSelector(form));
+        }
+        if (!form || form.nodeName !== 'FORM') {
+            throw 'Expected the HTML form element or the style selector of form';
+        }
+        UI_1.UI.progressState(form);
+        var formData = new FormData(form);
+        formData.append('lib_version', '2.6.3');
+        formData.append('service_id', serviceID);
+        formData.append('template_id', templateID);
+        formData.append('user_id', userID || _userID);
+        return sendPost(_origin + '/api/v1.0/email/send-form', formData)
+            .then(function (response) {
+            UI_1.UI.successState(form);
+            return response;
+        }, function (error) {
+            UI_1.UI.errorState(form);
+            return Promise.reject(error);
+        });
+    }
+    exports.sendForm = sendForm;
+    exports.default = {
+        init: init,
+        send: send,
+        sendForm: sendForm
+    };
+    });
+
     /* src/JoinMailingList.svelte generated by Svelte v3.29.6 */
+
+    const { console: console_1 } = globals;
     const file$1 = "src/JoinMailingList.svelte";
 
-    // (38:0) {:else}
+    // (57:0) {:else}
     function create_else_block$1(ctx) {
     	let button;
     	let mounted;
@@ -2057,13 +2241,13 @@ var app = (function () {
     			button = element("button");
     			button.textContent = "Join Mailing List";
     			attr_dev(button, "class", "button is-primary");
-    			add_location(button, file$1, 38, 2, 693);
+    			add_location(button, file$1, 57, 2, 1315);
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, button, anchor);
 
     			if (!mounted) {
-    				dispose = listen_dev(button, "click", /*click_handler*/ ctx[5], false, false, false);
+    				dispose = listen_dev(button, "click", /*click_handler_1*/ ctx[7], false, false, false);
     				mounted = true;
     			}
     		},
@@ -2079,14 +2263,14 @@ var app = (function () {
     		block,
     		id: create_else_block$1.name,
     		type: "else",
-    		source: "(38:0) {:else}",
+    		source: "(57:0) {:else}",
     		ctx
     	});
 
     	return block;
     }
 
-    // (24:0) {#if isOpen}
+    // (27:0) {#if isOpen}
     function create_if_block$1(ctx) {
     	let div;
     	let input0;
@@ -2094,6 +2278,7 @@ var app = (function () {
     	let input1;
     	let t1;
     	let button;
+    	let t2;
     	let mounted;
     	let dispose;
 
@@ -2105,46 +2290,53 @@ var app = (function () {
     			input1 = element("input");
     			t1 = space();
     			button = element("button");
-    			button.textContent = "Join Mailing List";
+    			t2 = text("Join Mailing List");
     			attr_dev(input0, "class", "input is-primary svelte-1vqeijs");
-    			attr_dev(input0, "type", "text");
+    			attr_dev(input0, "type", "name");
     			attr_dev(input0, "placeholder", "Name");
-    			add_location(input0, file$1, 25, 4, 376);
+    			add_location(input0, file$1, 28, 4, 437);
     			attr_dev(input1, "class", "input is-primary svelte-1vqeijs");
-    			attr_dev(input1, "type", "text");
+    			attr_dev(input1, "type", "email");
     			attr_dev(input1, "placeholder", "Email");
-    			add_location(input1, file$1, 30, 4, 494);
+    			add_location(input1, file$1, 33, 4, 555);
+    			button.disabled = /*sending*/ ctx[2];
     			attr_dev(button, "class", "button is-primary");
-    			add_location(button, file$1, 35, 4, 613);
+    			add_location(button, file$1, 38, 4, 676);
     			attr_dev(div, "class", "bottom-holder svelte-1vqeijs");
-    			add_location(div, file$1, 24, 2, 344);
+    			add_location(div, file$1, 27, 2, 405);
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, div, anchor);
     			append_dev(div, input0);
-    			set_input_value(input0, /*$user*/ ctx[2].name);
+    			set_input_value(input0, /*$user*/ ctx[3].name);
     			append_dev(div, t0);
     			append_dev(div, input1);
-    			set_input_value(input1, /*$user*/ ctx[2].name);
+    			set_input_value(input1, /*$user*/ ctx[3].email);
     			append_dev(div, t1);
     			append_dev(div, button);
+    			append_dev(button, t2);
 
     			if (!mounted) {
     				dispose = [
-    					listen_dev(input0, "input", /*input0_input_handler*/ ctx[3]),
-    					listen_dev(input1, "input", /*input1_input_handler*/ ctx[4])
+    					listen_dev(input0, "input", /*input0_input_handler*/ ctx[4]),
+    					listen_dev(input1, "input", /*input1_input_handler*/ ctx[5]),
+    					listen_dev(button, "click", /*click_handler*/ ctx[6], false, false, false)
     				];
 
     				mounted = true;
     			}
     		},
     		p: function update(ctx, dirty) {
-    			if (dirty & /*$user*/ 4 && input0.value !== /*$user*/ ctx[2].name) {
-    				set_input_value(input0, /*$user*/ ctx[2].name);
+    			if (dirty & /*$user*/ 8) {
+    				set_input_value(input0, /*$user*/ ctx[3].name);
     			}
 
-    			if (dirty & /*$user*/ 4 && input1.value !== /*$user*/ ctx[2].name) {
-    				set_input_value(input1, /*$user*/ ctx[2].name);
+    			if (dirty & /*$user*/ 8 && input1.value !== /*$user*/ ctx[3].email) {
+    				set_input_value(input1, /*$user*/ ctx[3].email);
+    			}
+
+    			if (dirty & /*sending*/ 4) {
+    				prop_dev(button, "disabled", /*sending*/ ctx[2]);
     			}
     		},
     		d: function destroy(detaching) {
@@ -2158,7 +2350,7 @@ var app = (function () {
     		block,
     		id: create_if_block$1.name,
     		type: "if",
-    		source: "(24:0) {#if isOpen}",
+    		source: "(27:0) {#if isOpen}",
     		ctx
     	});
 
@@ -2223,7 +2415,7 @@ var app = (function () {
     function instance$3($$self, $$props, $$invalidate) {
     	let $user,
     		$$unsubscribe_user = noop,
-    		$$subscribe_user = () => ($$unsubscribe_user(), $$unsubscribe_user = subscribe(user, $$value => $$invalidate(2, $user = $$value)), user);
+    		$$subscribe_user = () => ($$unsubscribe_user(), $$unsubscribe_user = subscribe(user, $$value => $$invalidate(3, $user = $$value)), user);
 
     	$$self.$$.on_destroy.push(() => $$unsubscribe_user());
     	let { $$slots: slots = {}, $$scope } = $$props;
@@ -2238,10 +2430,11 @@ var app = (function () {
     	validate_store(user, "user");
     	$$subscribe_user();
     	let isOpen = true;
+    	let sending = false;
     	const writable_props = [];
 
     	Object.keys($$props).forEach(key => {
-    		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== "$$") console.warn(`<JoinMailingList> was created with unknown prop '${key}'`);
+    		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== "$$") console_1.warn(`<JoinMailingList> was created with unknown prop '${key}'`);
     	});
 
     	function input0_input_handler() {
@@ -2250,25 +2443,64 @@ var app = (function () {
     	}
 
     	function input1_input_handler() {
-    		$user.name = this.value;
+    		$user.email = this.value;
     		user.set($user);
     	}
 
     	const click_handler = () => {
+    		$$invalidate(2, sending = true);
+
+    		source.send(
+    			process.env.service_id,
+    			process.env.template_id,
+    			{
+    				from_name: $user.name,
+    				message: $user.email
+    			},
+    			process.env.emailjs_user_id
+    		).then(
+    			response => {
+    				$$invalidate(2, sending = false);
+    				console.log("SUCCESS!", response.status, response.text);
+    			},
+    			err => {
+    				console.log("FAILED...", err);
+    			}
+    		);
+    	};
+
+    	const click_handler_1 = () => {
     		$$invalidate(1, isOpen = !isOpen);
     	};
 
-    	$$self.$capture_state = () => ({ writable, user, isOpen, $user });
+    	$$self.$capture_state = () => ({
+    		writable,
+    		emailjs: source,
+    		user,
+    		isOpen,
+    		sending,
+    		$user
+    	});
 
     	$$self.$inject_state = $$props => {
     		if ("isOpen" in $$props) $$invalidate(1, isOpen = $$props.isOpen);
+    		if ("sending" in $$props) $$invalidate(2, sending = $$props.sending);
     	};
 
     	if ($$props && "$$inject" in $$props) {
     		$$self.$inject_state($$props.$$inject);
     	}
 
-    	return [user, isOpen, $user, input0_input_handler, input1_input_handler, click_handler];
+    	return [
+    		user,
+    		isOpen,
+    		sending,
+    		$user,
+    		input0_input_handler,
+    		input1_input_handler,
+    		click_handler,
+    		click_handler_1
+    	];
     }
 
     class JoinMailingList extends SvelteComponentDev {
--- src/JoinMailingList.svelte
+++ src/JoinMailingList.svelte
@@ -1,12 +1,15 @@
 <script>
   import { writable } from "svelte/store";
+  import emailjs from "emailjs-com";
 
   export const user = writable({
     name: "",
     // phone: "11 972393003",
     email: "",
   });
+
   let isOpen = true;
+  let sending = false;
 </script>
 
 <style>
@@ -25,15 +28,31 @@
   <div class="bottom-holder">
     <input
       class="input is-primary"
-      type="text"
+      type="name"
       placeholder="Name"
       bind:value={$user.name} />
     <input
       class="input is-primary"
-      type="text"
+      type="email"
       placeholder="Email"
-      bind:value={$user.name} />
-    <button class="button is-primary">Join Mailing List</button>
+      bind:value={$user.email} />
+    <button
+      disabled={sending}
+      on:click={() => {
+        const templateParams = { name: 'James', notes: 'Check this out!' };
+        sending = true;
+        emailjs
+          .send(process.env.service_id, process.env.template_id,
+
+            { from_name: $user.name, message: $user.email }, process.env.emailjs_user_id)
+          .then( (response) => {
+              sending = false;
+              console.log('SUCCESS!', response.status, response.text);
+            }, (err) => {
+              console.log('FAILED...', err);
+            } );
+      }}
+      class="button is-primary">Join Mailing List</button>
   </div>
 {:else}
   <button
